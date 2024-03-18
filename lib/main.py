7c3116eceabb2ccd2be82408cff47053966fce37
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-18 08:51:26 +0300

    Completed challenge

--- lib/main.py
+++ lib/main.py
@@ -1,2 +1,18 @@
-def scoreboard():
-    pass
+def score(participant):
+    return participant["chickenwings"] * 5 + participant["hamburgers"] * 3 + participant["hotdogs"] * 2
+
+def create_scoreboard(participants):
+    scoreboard = []
+    for participant in participants:
+        scoreboard.append({"name": participant["name"], "score":score(participant)})
+    scoreboard.sort(key=lambda x: (-x["score"], x["name"]))
+    return scoreboard
+
+
+participants = [
+  {"name": "Habanero Hillary", "chickenwings": 5, "hamburgers": 17, "hotdogs": 11},
+  {"name": "Big Bob", "chickenwings": 20, "hamburgers": 4, "hotdogs": 11}
+]
+
+scoreboard = create_scoreboard(participants)
+print(scoreboard)
